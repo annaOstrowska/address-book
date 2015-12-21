740bbfd59ac2d222d9ca225b7c6a003faf50d61e
annaOstrowska <anna.ostrowska@moonpig.com>
2015-12-21 15:21:27 +0000

    added AddressBook class and some tests

--- src/main/java/book.java
+++
@@ -1,5 +0,0 @@
-public class book {
-    public void read(){
-        System.out.println("Hello World");
-    }
-}
---
+++ src/test/java/AddressBookTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by anna.ostrowska on 21/12/2015.
+ */
+public class AddressBookTest {
+}
