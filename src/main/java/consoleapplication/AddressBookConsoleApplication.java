7fe16f9e8865d0f5aadf05e40de3189b3a035e76
annaOstrowska <anna.ostrowska@moonpig.com>
2015-12-22 16:07:19 +0000

    :):)

--- src/main/java/consoleapplication/AddressBookConsoleApplication.java
+++ src/main/java/consoleapplication/AddressBookConsoleApplication.java
@@ -21,39 +21,41 @@ public class AddressBookConsoleApplication {
         System.out.println("ListContacts.");
         System.out.println("Exit Address Book.");
 
+
+
         boolean exit = false;
         do {
 
-        String inputLine = reader.read();
-        String[] words = inputLine.split(" ");
+            String inputLine = reader.read();
+            String[] words = inputLine.split(" ");
 
-        String command = words[0];
-        switch(command)
-        {
-            case "AddContact":
-                app.addContact();
-                break;
+            String command = words[0];
+            switch(command)
+            {
+                case "AddContact":
+                   app.addContact()
+                   ;break;
 
-            case "DeleteContact":
-                app.deleteContact();
-                break;
+                case "DeleteContact":
+                    app.deleteContact();
+                    break;
 
-            case "ListContacts":
-                IWriter terminalWriter = new TerminalWriter();
-                app.outputContacts(terminalWriter);
-                break;
+                case "ListContacts":
+                    IWriter terminalWriter = new TerminalWriter();
+                    app.outputContacts(terminalWriter);
+                    break;
 
-            case "Exit":
-                exit = true;
-                IWriter fileWriter = new MyFileWriter();
-                app.outputContacts(fileWriter);
-                break;
+                case "Exit":
+                    exit = true;
+                    IWriter fileWriter = new MyFileWriter();
+                    app.outputContacts(fileWriter);
+                    break;
 
-            default:
-                System.out.println("The system did not get your command.");
-                break;
+                default:
+                    System.out.println("The system did not get your command.");
+                    break;
 
-        }
+            }
             }
 
         while(!exit);
@@ -81,8 +83,22 @@ public class AddressBookConsoleApplication {
     }
 
     private void addContact() {
-        
+        TerminalReader reader = new TerminalReader();
+
+        System.out.println("What is the first name?");
+        String firstName = reader.read();
+        System.out.println("What is the last name?");
+        String lastName = reader.read();
+        System.out.println("What is the address?");
+        String homeAddress = reader.read();
+        System.out.println("What is the phone number?");
+        String phoneNumber = reader.read();
+
+        Contact contact = new Contact(firstName, lastName, homeAddress, phoneNumber);
+        addressBook.addContact(contact);
+
     }
+
     private void outputContacts(IWriter writer) {
         writer.outputAddressBook(addressBook.getAddressBookAsString());
     }
